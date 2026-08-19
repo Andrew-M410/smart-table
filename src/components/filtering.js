2327439ef665e883d76532d8c53671324e79876b
Andrew-M410 <andrewmaksimov410@yandex.ru>
2026-08-19 12:54:16 +0300

    PR7

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,21 +1,17 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+export function initFiltering(elements) {
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
+                const el = document.createElement('option');
+                el.textContent = name;
+                el.value = name;
+                return el;
+            }))
+        })
+    }
 
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
-
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes).forEach((elementName) => {                                         // Получаем ключи из объекта и Перебираем по именам
-        elements[elementName].append(...Object.values(indexes[elementName]).map(name => {   // в каждый элемент добавляем опции // формируем массив имён, значений опций // используйте name как значение и текстовое содержимое
-            // @todo: создать и вернуть тег опции
-            const elemTag = document.createElement('option');
-            elemTag.textContent = name;
-            elemTag.value = name;
-            return elemTag;
-        }))
-     })
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+    const applyFiltering = (query, state, action) => {
+        // код с обработкой очистки поля
         const applyFiltering = (query, state, action) => {
             if (action && action.name == 'clear') {
                 const parent = action.parentElement;
@@ -25,8 +21,23 @@ export function initFiltering(elements, indexes) {
                 state[field] = '';
             }
         }
+         
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state));
+        // @todo: #4.5 — отфильтровать данные, используя компаратор
+        const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) { // ищем поля ввода в фильтре с непустыми данными
+                    filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
+                }
+            }
+        })
+
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query; // если в фильтре что-то добавилось, применим к запросу
+    }
+
+    return {
+        updateIndexes,
+        applyFiltering
     }
 }
--- src/components/searching.js
+++ src/components/searching.js
@@ -1,20 +1,7 @@
-import {rules, createComparison} from "../lib/compare.js";
-
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
-    const compare = createComparison([
-        rules.skipEmptyTargetValues,
-        rules.searchMultipleFields(
-            searchField,
-            ["date", "customer", "seller"],
-            false,
-        ),
-    ]);
-
-    // @todo: #5.2 — применить компаратор
-    return (data, state) => {
-        const value = state[searchField];
-        if (!value) return data; // если поле пустое, возвращаем все строки
-        return data.filter((row) => compare(row, state));
-    };
+    return (query, state, action) => { // result заменили на query
+        return state[searchField] ? Object.assign({}, query, { // проверяем, что в поле поиска было что-то введено
+            search: state[searchField] // устанавливаем в query параметр
+        }) : query; // если поле с поиском пустое, просто возвращаем query без изменений
+    }
 }
